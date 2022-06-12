e4e65cfe9e24a7f6bc706111a57420bebeeb5858
Jan <jan.hanack@gmail.com>
2022-06-12 13:02:10 +0200

    Add JSON format String Test Query Output for PG and MDB

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,23 +3,27 @@ import java.sql.*;
 public class Test {
 
     private static Connection dbConn1;
+    private static Connection dbConn2;
 
     public static void main(String[] args) throws SQLException{
 
         dbConn1 = DriverManager.getConnection("jdbc:postgresql://localhost:8080/impro_database", "impro_user", "impro_password");
 
         Statement stmt = dbConn1.createStatement();
-        stmt.execute("EXPLAIN SELECT * FROM \"sf1_orders\" WHERE o_totalprice > 20");
+        stmt.execute("EXPLAIN (FORMAT JSON) SELECT * FROM \"sf1_orders\" WHERE o_totalprice > 20");
         ResultSet resultSet = stmt.getResultSet();
         resultSet.next();
         String retrievedName = resultSet.getString(1);
         System.out.println(retrievedName);
-        resultSet.next();
-        String retrievedName2 = resultSet.getString(1);
+        stmt.close();
+
+        dbConn2 = DriverManager.getConnection("jdbc:mariadb://localhost:8083/impro_database", "impro_user", "impro_password");
+        Statement stmt2 = dbConn2.createStatement();
+        stmt2.execute("EXPLAIN FORMAT=JSON SELECT * FROM sf1_orders WHERE o_totalprice > 20");
+        ResultSet resultSet2 = stmt2.getResultSet();
+        resultSet2.next();
+        String retrievedName2 = resultSet2.getString(1);
         System.out.println(retrievedName2);
-        resultSet.next();
-        String retrievedName3 = resultSet.getString(1);
-        System.out.println(retrievedName3);
         stmt.close();
     }
 }
