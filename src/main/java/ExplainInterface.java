2bb246a2b9e2c3f8057501bd35bd0295ad936cf5
Jan <jan.hanack@gmail.com>
2022-07-02 23:31:35 +0200

    Update Abstract Interface, for functions calling themselves

--- src/main/java/ExplainInterface.java
+++ src/main/java/ExplainInterface.java
@@ -1,6 +1,14 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public interface ExplainInterface {
-    public String getExplainOutput();
-    public String preprocessExplainString(String originalExplainString);
-    public String processExplainNodes(String originalExplainString);
-    public String explainToIR(String originalExplainString);
+    public JsonNode addExplainToQuery(String query) throws SQLException, IOException;
+    public JsonNode getExplainOutput(String query) throws SQLException, IOException;
+    public JsonNode preprocessExplainString(String originalExplainString) throws JsonProcessingException;
+    public JsonNode processExplainNodes(String originalExplainString) throws JsonProcessingException;
+    public void addLinks(JsonNode plan, int nodecount, int childcount) throws JsonProcessingException;
+    public JsonNode explainToIR(String originalExplainString);
 }
