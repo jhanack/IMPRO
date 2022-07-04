907f1c81296169e858fd14dfef1e1945583c28ee
Jan <jan.hanack@gmail.com>
2022-07-04 12:22:45 +0200

    Add TPC-H Query 4

--- src/test/java/PGRun.java
+++ src/test/java/PGRun.java
@@ -11,11 +11,11 @@ import java.sql.SQLException;
 
 public class PGRun {
     public static void main(String[] args) throws IOException, SQLException {
-        Path queryString = Path.of("resources/queries/q2_sf1.txt");
+        Path queryString = Path.of("resources/queries/q4_sf1.txt");
         String query = Files.readString(queryString);
         Postgres PGRun = new Postgres();
         ObjectMapper objectMapper = new ObjectMapper();
         ObjectWriter writer = objectMapper.writer(new DefaultPrettyPrinter());
-        writer.writeValue(new File("resources/IR-JSON-outputs/Q2_PG.json"), PGRun.addExplainToQuery(query));
+        writer.writeValue(new File("resources/IR-JSON-outputs/Q4_PG.json"), PGRun.addExplainToQuery(query));
     }
 }
